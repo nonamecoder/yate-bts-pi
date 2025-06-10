ca7a2bd2c2a4785f73fe1c05ed538aa9df12a9cf
Ayyappan <ayyappan.rajesh16@gmail.com>
2025-06-10 16:21:01 -0400

    Update pysim_install.sh
    Added break system packages flag for latest Pi image

--- pysim_install.sh
+++ pysim_install.sh
@@ -12,8 +12,8 @@ else
     echo "Python virtual environment is active: $VIRTUAL_ENV"
 fi
 pip3 uninstall -y pycrypto
-pip3 install pycryptodome pyscard setuptools
-pip3 install -r requirements.txt
+pip3 install --break-system-packages pycryptodome pyscard setuptools
+pip3 install --break-system-packages -r requirements.txt
 echo "pySim has been installed successfully."
 echo "Here is an example usage of pySim (can be used to program your first SIM):"
 echo "./pySim-prog.py -p 0 -n YateBTS -c 1 -x 310 -y 260 -i 310260000000551 -s 8912600000000005512 -o 659BDA03311ACBBE767CB56D565A58D6 -k BA351F5C4690491D86377319E5A6DBCC"
